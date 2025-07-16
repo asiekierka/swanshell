85690cf8792614c071ceb7203d47aa41a3daf081
Adrian Siekierka <kontakt@asie.pl>
2025-07-16 16:57:13 +0200

    ui_hidctrl: add basic visual indicator

--- src/menu/plugin/ui_hid.c
+++ src/menu/plugin/ui_hid.c
@@ -17,21 +17,41 @@
 
 #include <wonderful.h>
 #include <ws.h>
+#include <ws/display.h>
+#include <ws/system.h>
 
 #include "../lang.h"
+#include "lang_gen.h"
 #include "mcu.h"
 #include "../main.h"
 #include "../ui/ui.h"
 #include "../util/input.h"
+#include "ui/bitmap.h"
 
 int ui_hidctrl(void) {
     ui_layout_bars();
     ui_draw_titlebar(lang_keys[LK_SUBMENU_OPTION_CONTROLLER_MODE]);
+    ui_draw_statusbar(NULL);
 
-    // TODO: draw any kind of visual indicator
-    // TODO: more than 75 Hz polling
+    bitmapfont_set_active_font(font16_bitmap);
 
     ws_system_model_t model = ws_system_get_model();
+
+    uint16_t s1 = LK_CONTROLLER_MODE_ACTIVE;
+    uint16_t s2 = model == WS_MODEL_PCV2 ? LK_CONTROLLER_MODE_PRESS_VIEW_TO_EXIT : LK_CONTROLLER_MODE_PRESS_SOUND_TO_EXIT;
+    uint16_t width1 = bitmapfont_get_string_width(lang_keys[s1], 65535);
+    uint16_t width2 = bitmapfont_get_string_width(lang_keys[s2], 65535);
+    bitmapfont_draw_string(&ui_bitmap,
+        (WS_DISPLAY_WIDTH_PIXELS - width1) >> 1,
+        (WS_DISPLAY_HEIGHT_PIXELS >> 1) - 20,
+        lang_keys[s1], 65535);
+    bitmapfont_draw_string(&ui_bitmap,
+        (WS_DISPLAY_WIDTH_PIXELS - width2) >> 1,
+        (WS_DISPLAY_HEIGHT_PIXELS >> 1) + 4,
+        lang_keys[s2], 65535);
+
+    // TODO: more than 75 Hz polling
+
     while (true) {
         wait_for_vblank();
         input_update();
